f2605dcabf222ed6861de94c63005e88d333ea5a
thomasdinh2k <work.dnstung@gmail.com>
2024-04-16 14:41:38 +0700

    adding stylesheet for navlist

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+const navListLi = document.querySelectorAll(".nav-list > li")
+
+navListLi.forEach( ( li ) => { 
+    li.addEventListener("click", (event) => { 
+        event.preventDefault();
+
+        li.classList.toggle("active");
+     })
+ })
