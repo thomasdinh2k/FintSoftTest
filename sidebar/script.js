66879a516dd6f9d7a3373c3fac81211978b92f16
thomasdinh2k <work.dnstung@gmail.com>
2024-04-17 09:45:51 +0700

    re-structure HTML file

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,11 +1,11 @@
 console.log("Hello World");
 
-const navListLi = document.querySelectorAll(".nav-list > li")
+// const navListLi = document.querySelectorAll(".nav-list > li")
 
-navListLi.forEach( ( li ) => { 
-    li.addEventListener("click", (event) => { 
-        event.preventDefault();
+// navListLi.forEach( ( li ) => { 
+//     li.addEventListener("click", (event) => { 
+//         event.preventDefault();
 
-        li.classList.toggle("active");
-     })
- })
+//         li.classList.toggle("active");
+//      })
+//  })
