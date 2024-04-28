c51e5402f03cc41482524adacabaa3cb8135c853
thomasdinh2k <work.dnstung@gmail.com>
2024-04-28 10:50:59 +0700

    use ARIA attr to open sub-menu

--- liverpool-fan-club/script.js
+++ liverpool-fan-club/script.js
@@ -1,13 +1,18 @@
+// Ensure jQuery is ready
 $(document).ready(function () {
-    $(".mobile.menu-items > li ").each( itemLi => {
-        
-        
-        
-        var targetedMenu = $(itemLi).find(".wrapper");
-        
-        $(itemLi).click(function (e) { 
-            e.preventDefault();
-            $(targetedMenu).toggle("show");
-        });
-    })
-});
+	// Set up click event on the <a> element with class "nav-links"
+	$("a.nav-links").click(function (event) {
+		event.preventDefault() // Prevent the default anchor click behavior
+
+		// Find the next sibling .wrapper of the clicked <a>
+		var $wrapper = $(this).next(".wrapper")
+
+		// Toggle the aria-hidden and aria-expanded attributes
+		var isExpanded = $wrapper.attr("aria-expanded") === "true"
+		$wrapper.attr("aria-expanded", !isExpanded)
+		$wrapper.attr("aria-hidden", isExpanded)
+
+		// Optionally toggle additional class for CSS styling if needed
+		// $wrapper.toggleClass('some-class');
+	})
+})
